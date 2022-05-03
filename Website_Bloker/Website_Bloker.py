23bd215ceec9cdf686f3e88900b2d871b613cb68
th-araujo <th.araujo@outlook.com>
2022-05-02 23:36:23 -0300

    Primeira parte do projeto

--- Website_Bloker/Website_Bloker.py
+++ Website_Bloker/Website_Bloker.py
@@ -1,3 +1,11 @@
 import time
 from datetime import date as dt
-hosts_path = r" /etc/hosts"
+hosts_path = r"/etc/hosts"
+host_temp = "hosts"
+redirect = "127.0.01"
+lista_de_web_sites = ["instagram.com", "instagram.com"] # podemos adicionar mais sites que desejamos bloquear
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year,
+                                                                          dt.now().month, dt.now().day,22)
+        print("Hey Dev Junior Horário de programar")
